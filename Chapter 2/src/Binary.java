03b22a8065224811ee499886c50d2dd233a241ea
1005327001 <1005327001@L815WD220033.edu.cbe.ab.ca>
2022-09-12 12:46:59 -0600

    12 46

--- Chapter 2/src/Binary.java
+++ Chapter 2/src/Binary.java
@@ -1,8 +1,10 @@
 
-public class Binary {
+public class Binary 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
 
 	}
 
