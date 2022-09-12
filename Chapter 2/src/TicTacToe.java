edf9c6c054fb649815a5adcb28a84fe7bccec390
1005327001 <1005327001@L815WD220033.edu.cbe.ab.ca>
2022-09-12 12:46:44 -0600

    12 46 pm

---
+++ Chapter 2/src/TicTacToe.java
@@ -0,0 +1,21 @@
+
+public abstract class TicTacToe 
+{
+
+	public static void main(String[] args) 
+	{
+	    System.out.println("         |         |");
+        System.out.println("         |         |");
+        System.out.println("         |         |");
+        System.out.println("-----------------------------");
+        System.out.println("         |         |");
+        System.out.println("         |    X    |");
+        System.out.println("         |         |");
+        System.out.println("-----------------------------");
+        System.out.println("         |         |");
+        System.out.println("         |         |");
+        System.out.println("         |         |");
+
+	}
+
+}
